abe40cd742df10e6bbb081bc7c86f96e2e45693e
pepedinho <2spii94@gmail.com>
2024-12-22 20:17:26 +0100

    try to add some typing and error gesture
    need to implement a symbol tables (hash map) to keep in memory all my already declared variables and spot unknown variables

--- src/ast/parsing.rs
+++ src/ast/parsing.rs
@@ -2,24 +2,68 @@ use std::fs;
 
 use super::structure::{BinearyExpr, Expr, FunctionCall, Literal, Programm, Stmt};
 
-fn parse_expr(expr: &str) -> Expr {
+fn parse_expr(expr: &str, type_key: Option<&str>) -> Result<Expr, String> {
+    if let Some(key) = type_key {
+        if key == "str" {
+            return Ok(Expr::Literal(Literal::Str(
+                expr[1..expr.len() - 1].to_string(),
+            )));
+        } else if key == "int" {
+            if expr.contains("+") || expr.contains("-") {
+                //binary expression
+                let parts: Vec<&str> = expr.split_whitespace().collect();
+                if parts.len() == 3 {
+                    let left = match parse_expr(parts[0], type_key) {
+                        Ok(l) => l,
+                        Err(e) => return Err(e),
+                    };
+                    let right = match parse_expr(parts[2], type_key) {
+                        Ok(r) => r,
+                        Err(e) => return Err(e),
+                    };
+                    return Ok(Expr::Binary(Box::new(BinearyExpr {
+                        left,
+                        operator: parts[1].to_string(),
+                        right,
+                    })));
+                }
+                return Ok(Expr::Variable(expr.to_string())); // default
+            }
+
+            if let Ok(i) = expr.parse::<i64>() {
+                return Ok(Expr::Literal(Literal::Integer(i)));
+            } else {
+                return Err(format!("Invalid type expected '{}' ! expr : {}", key, expr));
+            }
+        }
+    }
     if expr.starts_with("\"") && expr.ends_with("\"") {
         // litteral String
-        Expr::Literal(Literal::Str(expr[1..expr.len() - 1].to_string()))
+        Ok(Expr::Literal(Literal::Str(
+            expr[1..expr.len() - 1].to_string(),
+        )))
     } else if let Ok(i) = expr.parse::<i64>() {
         // litteral integer
-        Expr::Literal(Literal::Integer(i))
+        Ok(Expr::Literal(Literal::Integer(i)))
     } else if expr.contains("+") || expr.contains("-") {
         //binary expression
         let parts: Vec<&str> = expr.split_whitespace().collect();
         if parts.len() == 3 {
-            return Expr::Binary(Box::new(BinearyExpr {
-                left: parse_expr(parts[0]),
+            let left = match parse_expr(parts[0], type_key) {
+                Ok(l) => l,
+                Err(e) => return Err(e),
+            };
+            let right = match parse_expr(parts[2], type_key) {
+                Ok(r) => r,
+                Err(e) => return Err(e),
+            };
+            return Ok(Expr::Binary(Box::new(BinearyExpr {
+                left,
                 operator: parts[1].to_string(),
-                right: parse_expr(parts[2]),
-            }));
+                right,
+            })));
         }
-        Expr::Variable(expr.to_string()) // default
+        Ok(Expr::Variable(expr.to_string())) // default
     } else if expr.contains("(") && expr.contains(")") {
         //function call
         let name_end = expr.find("(").unwrap();
@@ -27,57 +71,81 @@ fn parse_expr(expr: &str) -> Expr {
         let args_content = &expr[name_end + 1..expr.len() - 1];
         let arguments = args_content
             .split(',')
-            .map(|arg| parse_expr(arg.trim()))
+            .map(|arg| parse_expr(arg.trim(), None).unwrap())
             .collect();
-        Expr::FunctionCall(Box::new(FunctionCall { name, arguments }))
+        Ok(Expr::FunctionCall(Box::new(FunctionCall {
+            name,
+            arguments,
+        })))
     } else {
         // variable
-        Expr::Variable(expr.to_string())
+        Ok(Expr::Variable(expr.to_string()))
     }
 }
 
-fn parse_statement(line: &str, n: usize) -> Option<Stmt> {
-    if line.contains("=") {
-        // assignement
-        let parts: Vec<&str> = line.split("=").map(str::trim).collect();
-        if parts.len() == 2 {
-            let name = parts[0].to_string();
-            let value = parse_expr(parts[1]);
-            return Some(Stmt::Assignment {
-                name,
-                value,
-                line: n,
-            });
+fn is_type_keyword(word: &str) -> bool {
+    matches!(word, "int" | "float" | "str" | "bool")
+}
+
+fn parse_statement(line: &str, n: usize) -> Result<Option<Stmt>, String> {
+    if let Some((type_key, rest)) = line.split_once(' ') {
+        if is_type_keyword(type_key) {
+            if line.contains("=") {
+                // assignement
+                let parts: Vec<&str> = rest.split("=").map(str::trim).collect();
+                if parts.len() == 2 {
+                    let name = parts[0].to_string();
+                    let value = match parse_expr(parts[1], Some(type_key)) {
+                        //ici
+                        Ok(l) => l,
+                        Err(e) => return Err(format!("line {}: {}", n, e)),
+                    };
+                    return Ok(Some(Stmt::Assignment {
+                        name,
+                        value,
+                        line: n,
+                    }));
+                }
+            }
         }
-    } else if line.starts_with("if") {
+    }
+    if line.starts_with("if") {
         // condition
-        let condition = parse_expr(&line[3..]);
-        return Some(Stmt::If {
+        let condition = parse_expr(&line[3..], None).unwrap();
+        return Ok(Some(Stmt::If {
             condition,
             body: vec![],
             line: n,
-        });
+        }));
     } else if line.contains("(") && line.contains(")") {
-        let expr = parse_expr(line);
-        return Some(Stmt::Expression {
+        let expr = parse_expr(line, None).unwrap();
+        return Ok(Some(Stmt::Expression {
             expression: expr,
             line: n,
-        });
+        }));
     }
-
-    None
+    Ok(None)
 }
 
 fn pars_prog(content: &str) -> Vec<Stmt> {
     let mut statements = Vec::new();
 
     for (i, line) in content.lines().enumerate() {
+        // iteration ligne par ligne
         if line.trim().is_empty() {
-            continue;
+            continue; // si vide on ignore
         }
 
-        if let Some(stmt) = parse_statement(line.trim(), i) {
-            statements.push(stmt);
+        match parse_statement(line.trim(), i) {
+            Ok(stmt) => {
+                if let Some(st) = stmt {
+                    statements.push(st);
+                }
+            }
+            Err(e) => {
+                println!("{}", e);
+                break;
+            }
         }
     }
     statements
